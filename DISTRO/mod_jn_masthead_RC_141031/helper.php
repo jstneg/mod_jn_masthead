edef05cb6e1e59c24f1a954a1b5f210b666859a2
J.S. Negley <jstneg@gmail.com>
2014-11-04 15:39:02 -0600

    first release candidate

---
+++ DISTRO/mod_jn_masthead_RC_141031/helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Helper class
+ */
+class modjnmasthead
+{
+
+    /**
+     * Retrieves the hello message
+     *
+     * @param array $params An object containing the module parameters
+     * @access public
+     */
+    public static function test($params) {
+        return 'Masthead Mod says Hello';
+    }
+
+}
+?>
