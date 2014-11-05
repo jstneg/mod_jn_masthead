1b844cb770615c75c1d819a718ec1ec133674f7f
jstneg <jstneg@gmail.com>
2014-11-05 15:05:29 -0600

    revisions 001

--- DEV/mod_jn_masthead.php
+++ DEV/mod_jn_masthead.php
@@ -1,8 +1,21 @@
 <?php
-/**
- * Entry Point for Single Image 1.0 module
- * @ J.S. Negley
- */
+/*
+jsN Masthead Module for Joomla 3.x, Load a simple masthead 
+Copyright (c) 2014 J.S. Negley LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 
 defined( '_JEXEC' ) or die( 'Restricted access' ); // no direct access allowed
 
--- DEV/tmpl/default.php
+++ DEV/tmpl/default.php
@@ -4,13 +4,11 @@ defined('_JEXEC') or die;
 
 $doc = JFactory::getDocument();
 if($bkgStretchF){
-
 	$doc->addScript( $doc->baseurl.'/modules/mod_jn_masthead/js/jquery.backstretch.min.js', 'text/javascript', false );
 
 	$addscript  = 'jQuery(document).ready(function(){'."\n";
 	$addscript .= ' jQuery(".mod-masthead-id-'.$module->id.'").backstretch("'.$doc->baseurl.'/'.$bkgSrc.'");'."\n";
 	$addscript .= "});\n\n"; 	
-
 	$doc->addScriptDeclaration($addscript);
 } else {
 	$addstyle = '.mod-masthead-id-'.$module->id.'{ background:url('.$doc->baseurl.'/'.$bkgSrc.') top center; }'; 
